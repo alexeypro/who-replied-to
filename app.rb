3a94130c9f3c050ac54d0fdd039d053cff6b38cd
Olexiy Prokhorenko <alex@qwe3.com>
2009-04-25 20:28:06 -0700

    Added couple view features

--- app.rb
+++ app.rb
@@ -15,14 +15,15 @@ get '/problem' do
 end
 
 post '/results' do
-  target_message = params[:targetmessage]
-  unless target_message == nil || target_message.empty?
-    all_match = (/twitter.com\/(.*?)\//).match(target_message.downcase)
-    username = all_match[1] unless all_match == nil
+  @target_message = params[:targetmessage]
+  unless @target_message == nil || @target_message.empty?
+    all_match = (/twitter.com\/(.*?)\//).match(@target_message.downcase)
+    @target_name = all_match[1] unless all_match == nil
     redirect '/problem' and return if all_match == nil
 
+    # here we get all replies to author of target message (7 days is the limit)
     all_links = Array.new
-    req = "http://search.twitter.com/search.atom?q=to:" + username + "&rpp=1000"
+    req = "http://search.twitter.com/search.atom?q=to:" + @target_name + "&rpp=1000"
     begin
       open(req) do |f|
     	  f.each_line do |line|
@@ -38,14 +39,45 @@ post '/results' do
       return
     end
     
+    # we want to get here the actual target message
+    req = @target_message
+    begin
+      open(req) do |f|
+        data = f.read
+        # get the body
+        body_match = (/<span class="entry-content">(.*?)<\/span>/).match(data) unless data == nil
+        @target_body = body_match[1].strip unless body_match == nil
+        @target_body = "N/A" if body_match = nil
+        # the body may contain @username, need to replace with good url
+        @target_body = @target_body.gsub(/@<a href=\"\//, "@<a href=\"http://twitter.com/")        
+      end
+    rescue
+      # we can ignore here too, at least we'll show smth :-)
+    end    
+    
     @all_reply_links = Array.new
+    @hash_with_body = {}
+    @hash_with_name = {}    
     all_links.each do |link|
       begin
         open(link) do |f|
           data = f.read
-          search_string = "<a href=\"" + target_message + "\">in reply to " + username + "</a>"
-          we_have_reply = data.downcase.index(search_string) unless data == nil
-          @all_reply_links.push link if we_have_reply != nil
+          # get the body
+          body_match = (/<span class="entry-content">(.*?)<\/span>/).match(data) unless data == nil
+          body_string = body_match[1].strip unless body_match == nil
+          body_string = "N/A" if body_match = nil
+          # the body (as it is a reply) has @username, need to replace with good url
+          body_string = body_string.gsub(/@<a href=\"\//, "@<a href=\"http://twitter.com/")
+          # get authors name
+          name_match = (/twitter.com\/(.*?)\//).match(link.downcase)
+          name_string = name_match[1].strip unless name_match == nil
+          name_string = "unknown" if name_match == nil                    
+          # now actually check if this is a reply
+          search_string = "<a href=\"" + @target_message + "\">in reply to " + @target_name + "</a>"
+          we_have_reply = data.downcase.index(search_string.downcase) unless data == nil
+          @all_reply_links.push link unless we_have_reply == nil
+          @hash_with_body[link] = body_string unless we_have_reply == nil
+          @hash_with_name[link] = name_string unless we_have_reply == nil          
       	end
       rescue
         # ignore here
